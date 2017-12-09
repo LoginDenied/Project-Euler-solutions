7d2428142dc5496fe7e6915d9c9ad55ff1330ada
LoginDenied <32119742+LoginDenied@users.noreply.github.com>
2017-12-10 02:53:54 +0800

    Update Program_9.cpp

--- Program_9.cpp
+++ Program_9.cpp
@@ -1,8 +1,18 @@
+// Problem 9 - 	Special Pythagorean triplet
+
+/*
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a2 + b2 = c2
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+*/
+
 #include <iostream>
 
 using namespace std;
-
-// Declarations
 
 int main()
 {
@@ -12,12 +22,12 @@ int main()
 		{
 			for (int c = 1; c <= 997; c++)
 			{
-				if (((a+b+c) == 1000) && ((a*a) + (b*b)) == (c*c) && (a < b) && (b < c))
+				if (((a + b + c) == 1000) && ((a*a) + (b*b)) == (c*c) && (a < b) && (b < c))
 				{
 					cout << a*b*c << endl;
 				}
 			}
 		}
 	}
-    return 0;
+	return 0;
 }
